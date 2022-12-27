fbf7bddb4800c21c9618d0f5dac675df87df2984
Adam Wright <adam.t.wright@outlook.com>
2022-12-27 18:38:58 +0000

    add logic to create index buffer

--- src/main.cpp
+++ src/main.cpp
@@ -74,9 +74,14 @@ struct Vertex {
 };
 
 const std::vector<Vertex> vertices = {
-  {{ 0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
-  {{ 0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f }},
-  {{ -0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f }}
+  {{ -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
+  {{ 0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }},
+  {{ 0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f }},
+  {{ -0.5f, 0.5f }, { 1.0f, 1.0f, 1.0f }}
+};
+
+const std::vector<uint16_t> indices = {
+  0, 1, 2, 2, 3, 0
 };
 
 struct QueueFamilyIndices {
@@ -129,6 +134,8 @@ class HelloTriangleApplication {
     bool framebufferResized;
     VkBuffer vertexBuffer;
     VkDeviceMemory vertexBufferMemory;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
 
     void initWindow() {
       glfwInit();
@@ -157,6 +164,7 @@ class HelloTriangleApplication {
       createFramebuffers();
       createCommandPool();
       createVertexBuffers();
+      createIndexBuffer();
       createCommandBuffers();
       createSyncObjects();
     }
@@ -172,6 +180,9 @@ class HelloTriangleApplication {
 
     void cleanup() {
       cleanupSwapChain();
+
+      vkDestroyBuffer(device, indexBuffer, nullptr);
+      vkFreeMemory(device, indexBufferMemory, nullptr);
 
       vkDestroyBuffer(device, vertexBuffer, nullptr);
       vkFreeMemory(device, vertexBufferMemory, nullptr);
@@ -794,6 +805,8 @@ class HelloTriangleApplication {
       VkDeviceSize offsets[] = { 0 };
       vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
 
+      vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+
       VkViewport viewport{};
       viewport.x = 0.0f;
       viewport.y = 0.0f;
@@ -808,7 +821,7 @@ class HelloTriangleApplication {
       scissor.extent = swapChainExtent;
       vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
 
-      vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+      vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
       vkCmdEndRenderPass(commandBuffer);
 
       if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
@@ -1055,6 +1068,27 @@ class HelloTriangleApplication {
       vkQueueWaitIdle(graphicsQueue);
 
       vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+    }
+
+    void createIndexBuffer() {
+      VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+      VkBuffer stagingBuffer;
+      VkDeviceMemory stagingBufferMemory;
+
+      createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+      void* data;
+      vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+      memcpy(data, indices.data(), bufferSize);
+      vkUnmapMemory(device, stagingBufferMemory);
+
+      createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+      copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+      vkDestroyBuffer(device, stagingBuffer, nullptr);
+      vkFreeMemory(device, stagingBufferMemory, nullptr);
     }
 };
 
