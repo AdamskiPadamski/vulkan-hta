e7a26e600ef32e37f4ad57afa48be34a65e49252
Adam Wright <adam.t.wright@outlook.com>
2022-12-27 12:44:18 +0000

    add logic to create vertex buffer

--- src/main.cpp
+++ src/main.cpp
@@ -127,6 +127,8 @@ class HelloTriangleApplication {
     std::vector<VkFence> inFlightFences;
     uint32_t currentFrame = 0;
     bool framebufferResized;
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory;
 
     void initWindow() {
       glfwInit();
@@ -154,6 +156,7 @@ class HelloTriangleApplication {
       createGraphicsPipeline();
       createFramebuffers();
       createCommandPool();
+      createVertexBuffers();
       createCommandBuffers();
       createSyncObjects();
     }
@@ -169,6 +172,9 @@ class HelloTriangleApplication {
 
     void cleanup() {
       cleanupSwapChain();
+
+      vkDestroyBuffer(device, vertexBuffer, nullptr);
+      vkFreeMemory(device, vertexBufferMemory, nullptr);
 
       for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
         vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
@@ -784,6 +790,10 @@ class HelloTriangleApplication {
       vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
       vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
 
+      VkBuffer vertexBuffers[] = { vertexBuffer };
+      VkDeviceSize offsets[] = { 0 };
+      vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
+
       VkViewport viewport{};
       viewport.x = 0.0f;
       viewport.y = 0.0f;
@@ -798,7 +808,7 @@ class HelloTriangleApplication {
       scissor.extent = swapChainExtent;
       vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
 
-      vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+      vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
       vkCmdEndRenderPass(commandBuffer);
 
       if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
@@ -949,6 +959,50 @@ class HelloTriangleApplication {
       }
 
       vkDestroySwapchainKHR(device, swapChain, nullptr);
+    }
+
+    void createVertexBuffers() {
+      VkBufferCreateInfo bufferInfo{};
+      bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+      bufferInfo.size = sizeof(vertices[0]) * vertices.size();
+      bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+      bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+      if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create vertex buffer!");
+      }
+
+      VkMemoryRequirements memRequirements{};
+      vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
+
+      VkMemoryAllocateInfo allocInfo{};
+      allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+      allocInfo.allocationSize = memRequirements.size;
+      allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+      if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
+        throw std::runtime_error("failed to allocate vertex buffer memory!");
+      }
+
+      vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+
+      void* data;
+      vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
+      memcpy(data, vertices.data(), bufferInfo.size);
+      vkUnmapMemory(device, vertexBufferMemory);
+    }
+
+    uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
+      VkPhysicalDeviceMemoryProperties memProperties;
+      vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+      for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+        if (typeFilter & (1 << i) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+          return i;
+        }
+      }
+
+      throw std::runtime_error("failed to find suitable memory type!");
     }
 };
 
